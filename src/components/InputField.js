e61a7e3c29ed5519b1eb4c9123023589cfa538f9
Shahidul Islam <ishahin56@gmail.com>
2022-11-03 20:36:46 +0600

    Added eye icon and show hide password function

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import eyeIcon from '../assets/images/eye-black.png';
 
 const InputField = (props) => {
+
+    const [isShow, setIsShow] = useState(false);
+
+    const handleShow = (e) => {
+        e.preventDefault();
+        setIsShow(!isShow);
+        console.log(e, isShow);
+    }
+
     const {isError, errorMessage, name, type, placeholder, handleOnchange, inputValue} = props;
     return (
         <div className="space-y-[5px] w-full">
             {props?.label &&
                 <label htmlFor={name} className="font-bold text-label text-xs">{props?.label}</label>
             }
-            <input name={name} type={type} placeholder={placeholder} className={`py-3 px-4 border ${isError ? 'border-theme-red' : 'border-theme-gray'} rounded-[10px] text-sm text-label font-normal w-full focus:outline-none focus:shadow-none focus:border-primary`} onChange={(e) => handleOnchange(e.target.value)} value={inputValue}/>
+            {
+                props?.icon ? (
+                <div className="w-full relative">
+                    <input name={name} type={isShow ? 'text' : type} placeholder={placeholder} className={`py-3 px-4 border ${isError ? 'border-theme-red' : 'border-theme-gray'} rounded-[10px] text-sm text-label font-normal w-full focus:outline-none focus:shadow-none focus:border-primary`} onChange={(e) => handleOnchange(e.target.value)} value={inputValue}/>
+                    <button type="button" className={`absolute right-4 top-3 w-6 h-6 cursor-pointer z-10 ${isShow && 'shownLine'}`} onClick={handleShow}>
+                        <img src={eyeIcon} alt="Eye icon" />
+                    </button>
+                </div>
+                ) : (
+                    <input name={name} type={type} placeholder={placeholder} className={`py-3 px-4 border ${isError ? 'border-theme-red' : 'border-theme-gray'} rounded-[10px] text-sm text-label font-normal w-full focus:outline-none focus:shadow-none focus:border-primary`} onChange={(e) => handleOnchange(e.target.value)} value={inputValue}/>
+                )
+            }
+            
             {isError &&
                 <span className="text-theme-red font-bold text-xs">{errorMessage}</span>
             }
--- src/pages/auth/ChangePassword.js
+++ src/pages/auth/ChangePassword.js
@@ -33,7 +33,7 @@ const Registration = () => {
       <form className="space-y-5" onSubmit={handleOnSubmit}>
         <InputField name="password" type="password" placeholder="*****" label="Password" handleOnchange={handlePasswordField} errorMessage="This Password is required" isError={isEmailError} inputValue={password} />
         <p className="text-label font-bold text-xs -mr-2">Min 10 characters, 1 lowercase, 1 uppercase, and 1 numeric.</p>
-        <InputField name="password" type="password" placeholder="re-enter password" label="Password (again)" handleOnchange={handleConfirmPasswordField} errorMessage="This Confirm Password is required" isError={isEmailError} inputValue={confirmPassword} />
+        <InputField name="password" type="password" placeholder="re-enter password" label="Password (again)" handleOnchange={handleConfirmPasswordField} errorMessage="This Confirm Password is required" isError={isEmailError} inputValue={confirmPassword} icon={true} />
         <div className="w-full h-[10px]"></div>
         <div className="w-full space-y-4">
           <Button styles="bg-theme-red hover:bg-theme-red-dark text-white disabled:bg-theme-gray disabled:text-disabled" text="Change Password" />
